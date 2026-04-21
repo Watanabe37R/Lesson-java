71d2db240f0f2475bfe03939eada8a71e2325ef2
Watanabe37R <ryota.watanabe@arsnv.co.jp>
2026-04-21 15:26:21 +0900

    第１回演習直前までのファイル

---
+++ src/jp/co/aforce/sample/Calculator.java
@@ -0,0 +1,14 @@
+package jp.co.aforce.sample;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		/*計算する*/
+		int a = 1;
+		int b = 2;
+		int c = a + b;//cは３になる
+
+		/*計算した結果を表示する*/
+		System.out.println("1+2=" + c);
+	}
+}
