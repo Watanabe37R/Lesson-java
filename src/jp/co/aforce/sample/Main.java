a27cef2cbce5605f4077dba37c1a34587ffe529f
Watanabe37R <ryota.watanabe@arsnv.co.jp>
2026-04-24 13:53:46 +0900

    講義で使用したファイル(たい焼きインスタンス)

---
+++ src/jp/co/aforce/sample/Main.java
@@ -0,0 +1,14 @@
+package jp.co.aforce.sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Taiyaki cleamTaiyaki =new Taiyaki();
+		cleamTaiyaki.setFilling("クリーム");
+		cleamTaiyaki.showFilling();
+		Taiyaki ankoTaiyaki =new Taiyaki();
+		ankoTaiyaki.setFilling("あんこ");
+		ankoTaiyaki.showFilling();
+	}
+
+}
