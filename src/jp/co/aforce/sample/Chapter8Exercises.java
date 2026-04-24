ad65808368a36ff413a2a68a3757116341901898
Watanabe37R <ryota.watanabe@arsnv.co.jp>
2026-04-24 10:55:14 +0900

    第８章演習完了(更新があったので念のためコミット)

--- src/jp/co/aforce/sample/Chapter8Exercises.java
+++ src/jp/co/aforce/sample/Chapter8Exercises.java
@@ -3,26 +3,49 @@ package jp.co.aforce.sample;
 public class Chapter8Exercises {
 
 	public static void main(String[] args) {
-		
+
 	}
 }
+
+//カプセル化の問２
+class PersonCapsule {
+	//String name;
+	private String name;
+	//int age;
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (age >= 0) {
+			this.age = age;
+		}
+	}
+	public String getName() {
+		return name;
+	}
+	//・・・
+}
+
 //継承の問２
 class Person {
-    String name;
-    int age;
+	String name;
+	int age;
 }
 
 class Student extends Person {
-    //String name;
-    //int age;
-    String studentId;
+	//String name;
+	//int age;
+	String studentId;
 }
 
 //ポリモーフィズムの問２
 class Animal {
-    void makeSound() {
-        System.out.println("Some sound...");
-    }
+	void makeSound() {
+		System.out.println("Some sound...");
+	}
 }
 
 class Dog extends Animal {
@@ -35,7 +58,6 @@ class Dog extends Animal {
 class Cat extends Animal {
 	@Override
 	void makeSound() {
-		System.out.println("nyan");
+		System.out.println("meow");
 	}
 }
-
